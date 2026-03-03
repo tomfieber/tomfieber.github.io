e300422dac9da0b6a2a8942b1f768f8c92f7c5d4
Tom Fieber <108031577+tomfieber@users.noreply.github.com>
2026-03-03 17:15:06 -0600

    Update site configuration for new author and domain

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://tomfieber.github.io/", // replace this with your deployed domain
+  author: "Tom Fieber",
+  profile: "https://github.com/tomfieber/",
+  desc: "My personal blog site",
+  title: "Lazy River Infosec",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,7 +12,7 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
