6d8b678308107a25b5a5eccd3ecc9c8925c7aa97
Tom Fieber <108031577+tomfieber@users.noreply.github.com>
2026-03-03 19:19:09 -0600

    Update about

--- src/constants.ts
+++ src/constants.ts
@@ -77,12 +77,6 @@ export const SHARE_LINKS: Social[] = [
     icon: IconTelegram,
   },
   {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
-  },
-  {
     name: "Mail",
     href: "mailto:?subject=See%20this%20post&body=",
     linkTitle: `Share this post via email`,
